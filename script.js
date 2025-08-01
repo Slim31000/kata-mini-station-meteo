759dedc431c53d0c38b161c2b20f11021ffa5512
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-08-01 11:02:36 +0200

    add script and update index.html

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-// A toi de jouer pour cette partie :-) Happy coding !
+
+
+const cityInput =document.getElementById('cityInput')
+
+const city = document.getElementById('city')
+const gps = document.getElementById('gps')
+const temperature = document.getElementById("temperature")
+const detail = document.getElementById("details")
+
+btnValidate=document.getElementById('validate')
+
+
+btnValidate.addEventListener('click',async ()=>{
+    
+    const position=await fetchCoordinates(cityInput.value)
+    if(position){
+        city.innerHTML=position.city
+        gps.innerHTML = `Coordonnées GPS ${position.coordinates.latitude}, ${position.coordinates.longitude}`
+        temperature.innerHTML=await fetchWeather(position.coordinates.latitude,position.coordinates.longitude)
+        detail.innerHTML= `Température actuelle`
+        
+    } else{
+        city.innerHTML =`Ville non trouvée`
+        gps.innerHTML = "-"
+        temperature.innerHTML = ""
+        detail.innerHTML = `Vérifiez le nom de la ville`
+
+    }
+    cityInput.value=''
+    
+})
+
+
+///url=`https://nominatim.openstreetmap.org/search?q=${city}&format=json&addressdetails=1&limit=1`
+
+async function fetchCoordinates(cityInput) {
+    try {
+        const response= await fetch(`https://nominatim.openstreetmap.org/search?q=${cityInput}&format=json&addressdetails=1&limit=1`)
+        const data= await response.json()
+        console.log(data)
+        const latitude= data[0].lat
+        const longitude = data[0].lon
+        const coordinates = {latitude,longitude}
+        const city = data[0].name
+        console.log({city,coordinates})
+        return {city,coordinates}
+    } catch (error) {
+        console.log(error)
+    }
+    
+    
+}
+
+async function fetchWeather(latitude,longitude) {
+
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation,relative_humidity_2m`)
+    const data = await response.json()
+    console.log(data.current.temperature_2m)
+    return data.current.temperature_2m
+}
+
